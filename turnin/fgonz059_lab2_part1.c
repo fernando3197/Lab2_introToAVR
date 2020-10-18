21ece4222ba7d9058de3a5a2fa28727ee4f1946a
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-17 21:48:04 -0700

    Finished lab 2's ex 1

---
+++ turnin/fgonz059_lab2_part1.c
@@ -0,0 +1,33 @@
+/*	Author: fgonz059
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize
+				   // to 0s
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of A
+	while (1) {
+		tmpA = PINA & 0x03;
+		
+		if (tmpA == 0x01){
+			tmpB = tmpA;
+		} else {
+			tmpB = tmpB & 0x00; 
+		}
+		
+		PORTB = tmpB;
+	}
+	return 0;
+}
