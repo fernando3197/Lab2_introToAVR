541ed0b9ab3dd8951abcb8151001817f277a6a23
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-18 02:03:35 -0700

    Committing because I think i finished

---
+++ turnin/fgonz059_lab2_part4.c
@@ -0,0 +1,39 @@
+/*	Author: fgonz059
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0X00; PORTC = 0xFF; // Configure port A's 8 pins as inputs
+	DDRD = 0xFF; PORTD = 0x00; // Configure port B's 8 pins as outputs, initialize
+		
+	unsigned char tempWeight = 0x00;
+	unsigned char tempA = 0x00;
+	unsigned char tempB = 0x00;
+	unsigned char tempC = 0x00;
+	unsigned char tempD = 0x00;
+	
+	while (1) {
+		tempA = PINA;
+		tempB = PINB;
+		tempC = PINC;
+		tempWeight = tempA + tempB + tempC;
+			
+		tempD = tempWeight > 140 ? tempD | 0x01 : tempD & 0xFE;
+		tempD = (tempA - tempC) > 80 ? tempD | 0x02 : tempD & 0xFD;
+		tempD = (tempD & 0x03) | (tempWeight & 0xFC);	
+		PORTD = tempD;
+	}
+	return 0;
+}
